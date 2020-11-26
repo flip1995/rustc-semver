e85f5d3300807cb9b114e0070a4c613074a39a18
flip1995 <philipp.krones@embecosm.com>
2020-11-26 18:49:54 +0100

    Refactor implementation and make it no_std

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,10 @@
-use core::{
-    cmp::Ordering,
-    convert::{TryFrom, TryInto},
-    fmt::Display,
-    num::ParseIntError,
-};
+#![no_std]
+use core::{cmp::Ordering, convert::TryFrom, fmt::Display, num::ParseIntError};
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum Error {
     TooManyElements,
-    TooFewElements,
-    TooManyElementsInRest,
-    RestOnNonZeroPatch,
+    NotASpecialVersion,
     ParseIntError,
 }
 
@@ -22,155 +16,170 @@ impl From<ParseIntError> for Error {
 
 type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Default, Eq, PartialEq)]
-pub struct RustVersion<'a> {
-    major: u32,
-    minor: Option<u32>,
-    patch: Option<u32>,
-    rest: Option<&'a str>,
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum RustVersion {
+    Normal { major: u32, minor: u32, patch: u32 },
+    Special(SpecialVersion),
 }
 
-impl PartialOrd for RustVersion<'_> {
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum SpecialVersion {
+    Alpha,
+    Alpha2,
+    Beta,
+}
+
+impl PartialOrd for RustVersion {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for RustVersion<'_> {
+impl Ord for RustVersion {
     fn cmp(&self, other: &Self) -> Ordering {
-        match self.major.cmp(&other.major) {
-            Ordering::Equal => match (self.minor, other.minor) {
-                (Some(minor), Some(o_minor)) => match minor.cmp(&o_minor) {
-                    Ordering::Equal => match (self.patch, other.patch) {
-                        (Some(patch), Some(o_patch)) => match patch.cmp(&o_patch) {
-                            Ordering::Equal if patch == 0 => match (self.rest, other.rest) {
-                                (Some(rest), Some(o_rest)) if rest == o_rest => Ordering::Equal,
-                                (Some(rest), Some(o_rest)) => match (rest, o_rest) {
-                                    ("alpha", _) | ("alpha.2", "beta") => Ordering::Less,
-                                    ("beta", _) | ("alpha.2", "alpha") => Ordering::Greater,
-                                    _ => {
-                                        unreachable!("This was already checked in the constructors")
-                                    }
-                                },
-                                (Some(_), None) => Ordering::Less,
-                                (None, Some(_)) => Ordering::Greater,
-                                (None, None) => Ordering::Equal,
-                            },
-                            o @ _ => o,
-                        },
-                        (Some(patch), None) => patch.cmp(&0),
-                        (None, Some(patch)) => 0.cmp(&patch),
-                        (None, None) => Ordering::Equal,
-                    },
-                    o @ _ => o,
+        match (self, other) {
+            (
+                Self::Normal {
+                    major,
+                    minor,
+                    patch,
                 },
-                (Some(minor), None) => minor.cmp(&0),
-                (None, Some(minor)) => 0.cmp(&minor),
-                (None, None) => Ordering::Equal,
+                Self::Normal {
+                    major: o_major,
+                    minor: o_minor,
+                    patch: o_patch,
+                },
+            ) => match major.cmp(&o_major) {
+                Ordering::Equal => match minor.cmp(&o_minor) {
+                    Ordering::Equal => patch.cmp(&o_patch),
+                    ord => ord,
+                },
+                ord => ord,
             },
-            o @ _ => o,
+            (Self::Normal { major, .. }, Self::Special(_)) => {
+                if *major >= 1 {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            }
+            (Self::Special(_), Self::Normal { major, .. }) => {
+                if *major >= 1 {
+                    Ordering::Less
+                } else {
+                    Ordering::Greater
+                }
+            }
+            (Self::Special(s_ver), Self::Special(o_s_ver)) => s_ver.cmp(o_s_ver),
         }
     }
 }
 
-impl Display for RustVersion<'_> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        if let Some(rest) = self.rest {
-            write!(
-                f,
-                "{}.{}.{}-{}",
-                self.major,
-                self.minor.unwrap_or(0),
-                self.patch.unwrap_or(0),
-                rest
-            )?;
-        } else {
-            write!(
-                f,
-                "{}.{}.{}",
-                self.major,
-                self.minor.unwrap_or(0),
-                self.patch.unwrap_or(0)
-            )?;
+impl PartialOrd for SpecialVersion {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for SpecialVersion {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Self::Alpha, Self::Alpha)
+            | (Self::Alpha2, Self::Alpha2)
+            | (Self::Beta, Self::Beta) => Ordering::Equal,
+            (Self::Alpha, _) | (Self::Alpha2, Self::Beta) => Ordering::Less,
+            (Self::Beta, _) | (Self::Alpha2, Self::Alpha) => Ordering::Greater,
         }
+    }
+}
+
+impl Display for RustVersion {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::Normal {
+                major,
+                minor,
+                patch,
+            } => write!(f, "{}.{}.{}", major, minor, patch)?,
+            Self::Special(special) => write!(f, "{}", special)?,
+        }
+
         Ok(())
     }
 }
 
-impl TryFrom<Vec<u32>> for RustVersion<'_> {
+impl Display for SpecialVersion {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::Alpha => write!(f, "1.0.0-alpha")?,
+            Self::Alpha2 => write!(f, "1.0.0-alpha.2")?,
+            Self::Beta => write!(f, "1.0.0-beta")?,
+        }
+
+        Ok(())
+    }
+}
+
+impl TryFrom<[u32; 3]> for RustVersion {
     type Error = Error;
 
-    fn try_from(arr: Vec<u32>) -> Result<Self> {
-        if arr.len() > 3 {
+    fn try_from(vec: [u32; 3]) -> Result<Self> {
+        if vec.len() > 3 {
             Err(Error::TooManyElements)
-        } else if arr.is_empty() {
-            Err(Error::TooFewElements)
         } else {
-            Ok(Self {
-                major: arr[0],
-                minor: arr.get(1).copied(),
-                patch: arr.get(2).copied(),
-                rest: None,
+            Ok(Self::Normal {
+                major: vec.get(0).copied().unwrap_or_default(),
+                minor: vec.get(1).copied().unwrap_or_default(),
+                patch: vec.get(2).copied().unwrap_or_default(),
             })
         }
     }
 }
 
-const ACCEPTED_REST: [&str; 3] = ["alpha", "alpha.2", "beta"];
+const ACCEPTED_SPECIAL_VERSIONS: [(&str, SpecialVersion); 3] = [
+    ("1.0.0-alpha", SpecialVersion::Alpha),
+    ("1.0.0-alpha.2", SpecialVersion::Alpha2),
+    ("1.0.0-beta", SpecialVersion::Beta),
+];
 
-impl<'a> RustVersion<'a> {
+impl RustVersion {
     pub const fn new(major: u32, minor: u32, patch: u32) -> Self {
-        Self {
+        Self::Normal {
             major,
-            minor: Some(minor),
-            patch: Some(patch),
-            rest: None,
+            minor,
+            patch,
         }
     }
 
-    pub fn new_with_rest(major: u32, minor: u32, patch: u32, rest: &'a str) -> Result<Self> {
-        if !ACCEPTED_REST.contains(&rest) || patch != 0 {
-            return Err(Error::RestOnNonZeroPatch);
+    pub fn parse(version: &str) -> Result<Self> {
+        let special_version = ACCEPTED_SPECIAL_VERSIONS.iter().find_map(|sv| {
+            if version == sv.0 {
+                Some(sv.1)
+            } else {
+                None
+            }
+        });
+        if let Some(special_version) = special_version {
+            return Ok(RustVersion::Special(special_version));
         }
-        Ok(Self {
-            major,
-            minor: Some(minor),
-            patch: Some(patch),
-            rest: Some(rest),
-        })
-    }
 
-    pub fn parse(version: &'a str) -> Result<Self> {
-        let mut rust_version = vec![];
-        let mut version_rest = None;
+        let mut rust_version = [0_u32; 3];
         for (i, part) in version.split('.').enumerate() {
             if i == 3 {
                 return Err(Error::TooManyElements);
             }
-            if i == 2 {
-                let mut patch_and_rest = part.split('-');
-                let patch = patch_and_rest.next();
-                let rest = patch_and_rest.next();
-                if patch_and_rest.next().is_some() {
-                    return Err(Error::TooManyElementsInRest);
-                }
-                if let Some(patch) = patch {
-                    let patch = str::parse(patch)?;
-                    rust_version.push(patch);
-                    if let Some(rest) = rest {
-                        if !ACCEPTED_REST.contains(&rest) || patch != 0 {
-                            return Err(Error::RestOnNonZeroPatch);
-                        }
-                        version_rest = Some(rest);
+            match str::parse(part) {
+                Ok(part) => rust_version[i] = part,
+                Err(e) => {
+                    if i == 2 {
+                        return Err(Error::NotASpecialVersion);
+                    } else {
+                        return Err(e.into());
                     }
                 }
-            } else {
-                rust_version.push(str::parse(part)?);
             }
         }
-        let mut rust_version: Self = rust_version.try_into()?;
-        rust_version.rest = version_rest;
-        Ok(rust_version)
+        RustVersion::try_from(rust_version)
     }
 }
 
@@ -179,7 +188,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn omitted_parts() {
         assert_eq!(
             RustVersion::parse("1.0.0").unwrap(),
             RustVersion::new(1, 0, 0)
@@ -189,5 +198,29 @@ mod test {
             RustVersion::new(1, 0, 0)
         );
         assert_eq!(RustVersion::parse("1").unwrap(), RustVersion::new(1, 0, 0));
+    }
+
+    #[test]
+    fn special_versions() {
+        assert_eq!(
+            RustVersion::parse("1.0.0-alpha").unwrap(),
+            RustVersion::Special(SpecialVersion::Alpha)
+        );
+        assert_eq!(
+            RustVersion::parse("1.0.0-alpha.2").unwrap(),
+            RustVersion::Special(SpecialVersion::Alpha2)
+        );
+        assert_eq!(
+            RustVersion::parse("1.0.0-beta").unwrap(),
+            RustVersion::Special(SpecialVersion::Beta)
+        );
+        assert_eq!(
+            RustVersion::parse("1.0.0-sigma"),
+            Err(Error::NotASpecialVersion)
+        );
+        assert_eq!(
+            RustVersion::parse("1.0.0beta"),
+            Err(Error::NotASpecialVersion)
+        );
     }
 }
